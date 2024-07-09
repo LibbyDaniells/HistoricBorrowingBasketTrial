dc4972e597587b257295d3a47da5f1448cbd4628
LibbyDaniells <101347040+LibbyDaniells@users.noreply.github.com>
2024-07-09 09:48:27 +0100

    Update and rename PowerPrior_Simulation.R to EXppNEX_Simulation.R

--- EXppNEX_Simulation.R
+++ EXppNEX_Simulation.R
@@ -6,167 +6,6 @@ library(LaplacesDemon)
 library(parallel)
 library(MASS)
 
-#Power prior in the NEX component--------------------------------------------------
-PowerPrior <- function(K,y,n,q0,pi,pw,yh,nh,alpha0,a,b){
-  nexmu <- rep(log(pw/(1-pw)),K)
-  nexsigma <- rep((1/pw)+(1/(1-pw)),K)
-  hist <- as.numeric(nh>0)
-  jags.data <- list('n'=n,'K'=K,'y'=y,'pi'=pi,'nexmu'=nexmu,'nexsigma'=nexsigma,
-                    'yh'=yh,'nh'=nh,'alpha0'=alpha0,'a'=a,'b'=b,'hist'=hist,'q0'=q0)
-  jags.fit <- jags.model(file='PowerPriorinNEX.txt',data=jags.data,n.adapt=100000,n.chains=4)
-  samples <- coda.samples(jags.fit,variable.names = c('p','weight'),n.iter=100000,silent=TRUE) #Fit the model
-  powerp <- as.data.frame(samples[[1]])
-  return(powerp)
-}
-
-#Calibration---------------------------------------
-PowerPrior_Cal <- function(K,p,n,q0,pi,pw,ph,nh,alpha0,a,b,run){
-  Delta.mat <- matrix(NA,nrow=run,ncol=K)
-  nexmu <- rep(log(pw/(1-pw)),K)
-  nexsigma <- rep((1/pw)+(1/(1-pw)),K)
-  hist <- as.numeric(nh>0)
-  post.prob.fun <- function(x){
-    fun<-sum(x>q0)/100000
-    return(fun)
-  }
-  for(i in 1:run){
-    y <- rbinom(K,n,p)
-    yh <- rbinom(K,nh,ph)
-    jags.data <- list('n'=n,'K'=K,'y'=y,'pi'=pi,'nexmu'=nexmu,'nexsigma'=nexsigma,
-                      'yh'=yh,'nh'=nh,'alpha0'=alpha0,'a'=a,'b'=b,'hist'=hist,'q0'=q0)
-    jags.fit <- jags.model(file='PowerPriorinNEX.txt',data=jags.data,n.adapt=1000,n.chains=4,quiet=T)
-    samples <- coda.samples(jags.fit,variable.names = c('p'),n.iter=100000,silent=TRUE) #Fit the model
-    powerp <- as.data.frame(samples[[1]])
-    Delta.mat[i,] <- apply(powerp,2,post.prob.fun)
-    print(i)
-  }
-  Delta <- colQuantiles(Delta.mat,probs=0.9)
-  return(Delta)
-}
-
-# 
-# K <- 5
-# p <- rep(0.1,K)
-# ph <- c(0.1,0.1,0.1,0,0)
-# nh <- c(13,13,13,0,0)
-# n <- rep(34,K)
-# q0 <- 0.1
-# run <- 10000
-# pw <- 0.2
-# pi <- rbind(c(0.5,0.5),c(0.5,0.5),c(0.5,0.5),c(0.5,0.5),c(0.5,0.5))
-# alpha0 <- 0.5
-# a <- 1
-# b <- 1
-# 
-# power.prior <- PowerPrior_Cal(K,p,n,q0,pi,pw,ph,nh,alpha0,a,b,run)
-# print(power.prior)
-# 
-# save(power.prior,file='Power_Prior_Delta.RData')
-
-
-# 
-# 
-# #Simulation-----------------------------
-# PowerPrior_Sim <- function(pmat,n,nh,K,q0,pw,pi,run,alpha0,a,b,powerprior.delta){
-#   H <- sum(nh!=0)
-#   p <- pmat[1:K]
-#   ph <- pmat[K+1:H]
-#   hist <- as.numeric(nh>0)
-#   hypo <- matrix(NA,nrow=run,ncol=K)
-#   true <- rep(0,K)
-#   pointests <- matrix(NA,nrow=run,ncol=K)
-#   nexmu <- rep(log(pw/(1-pw)),K)
-#   nexsigma <- rep((1/pw)+(1/(1-pw)),K)
-#   for(t in 1:K){
-#     if(p[t]<=q0){true[t]<-0}else{true[t]<-1}
-#   }
-#   post.prob.fun <- function(x){
-#     fun<-sum(x>q0)/100000
-#     return(fun)
-#   }
-#   for(i in 1:run){
-#     y <- rbinom(K,n,p)
-#     yh <- rbinom(K,nh,ph)
-#     jags.data <- list('n'=n,'K'=K,'y'=y,'pi'=pi,'nexmu'=nexmu,'nexsigma'=nexsigma,
-#                       'yh'=yh,'nh'=nh,'alpha0'=alpha0,'a'=a,'b'=b,'hist'=hist,'q0'=q0)
-#     jags.fit <- jags.model(file='PowerPriorinNEX.txt',data=jags.data,n.adapt=1000,n.chains=4,quiet=T)
-#     samples <- coda.samples(jags.fit,variable.names = c('p'),n.iter=100000,silent=TRUE) #Fit the model
-#     powerp <- as.data.frame(samples[[1]])
-#     pointests[i,] <- colMeans(powerp)
-#     hypo[i,] <- as.integer(apply(powerp,2,post.prob.fun)>powerprior.delta)
-#     print(i)
-#   }
-#   perfect <- 0 
-#   for(j in 1:run){
-#     if(all(hypo[j,]==true)){perfect<-perfect+1}
-#   }
-#   fwer_true <- which(true==0)
-#   if(sum(true)==K){fwer <- rep('NA',K)}else{
-#     fwer <- 0
-#     for(a in 1:run){
-#       error <- rep(0,length(fwer_true))
-#       for(b in 1:length(fwer_true)){
-#         if(hypo[a,fwer_true[b]]==1){
-#           error[b] <- 1
-#         }else{error[b] <- 0}
-#       }
-#       if(sum(error)!=0){fwer <- fwer+1}
-#     }
-#     fwer <- fwer/run
-#   }
-#   Pointmeans <- colMeans(pointests)
-#   Pointsds <- apply(pointests,2,sd)
-#   my_list <- list('Hypo'=hypo,'Point Estimates'=pointests,'Point Estimate Means'=Pointmeans,'Point Estimate Sds'=Pointsds,'Error Rates'=colMeans(hypo)*100,'Perfect'=perfect/run,'FWER'=fwer)
-#   return(my_list)
-# }
-
-
-
-
-# K <- 5
-# n <- rep(34,K)
-# nh <- c(13,13,13,0,0)
-# q0 <- 0.1
-# powerprior.delta <- 0.8608096
-# run <- 10000
-# pw <- 0.2
-# alpha0 <- 0.5
-# a <- 1
-# b <- 1
-# pi <- rbind(c(0.5,0.5),c(0.5,0.5),c(0.5,0.5),c(0.5,0.5),c(0.5,0.5))
-# 
-# p1 <- c(c(0.1,0.1,0.1,0.1,0.1),c(0.1,0.1,0.1))
-# p2 <- c(c(0.1,0.1,0.1,0.1,0.1),c(0.25,0.1,0.1))
-# p3 <- c(c(0.1,0.1,0.1,0.1,0.1),c(0.25,0.25,0.25))
-# p4 <- c(c(0.25,0.1,0.1,0.1,0.1),c(0.25,0.1,0.1))
-# p5 <- c(c(0.25,0.1,0.1,0.1,0.1),c(0.25,0.25,0.1))
-# p6 <- c(c(0.25,0.1,0.1,0.1,0.1),c(0.25,0.25,0.25))
-# p7 <- c(c(0.25,0.25,0.1,0.1,0.1),c(0.25,0.25,0.1))
-# p8 <- c(c(0.25,0.25,0.1,0.1,0.1),c(0.25,0.1,0.1))
-# p9 <- c(c(0.25,0.25,0.1,0.1,0.1),c(0.25,0.25,0.25))
-# p10 <- c(c(0.25,0.25,0.25,0.1,0.1),c(0.25,0.25,0.25))
-# p11 <- c(c(0.25,0.25,0.25,0.1,0.1),c(0.25,0.25,0.1))
-# p12 <- c(c(0.25,0.25,0.25,0.1,0.1),c(0.1,0.1,0.1))
-# p13 <- c(c(0.25,0.25,0.25,0.25,0.1),c(0.25,0.25,0.25))
-# p14 <- c(c(0.25,0.25,0.25,0.25,0.1),c(0.25,0.25,0.1))
-# p15 <- c(c(0.25,0.25,0.25,0.25,0.1),c(0.1,0.1,0.1))
-# p16 <- c(c(0.25,0.25,0.25,0.25,0.25),c(0.25,0.25,0.25))
-# p17 <- c(c(0.25,0.25,0.25,0.25,0.25),c(0.25,0.25,0.1))
-# p18 <- c(c(0.25,0.25,0.25,0.25,0.25),c(0.1,0.1,0.1))
-# 
-# list.scenarios1 <- list(p1,p2,p3,p4,p5,p6,p7,p8,p9)
-# list.scenarios2 <- list(p10,p11,p12,p13,p14,p15,p16,p17,p18)
-# 
-# powerprior.sim1_9 <- mclapply(list.scenarios1,PowerPrior_Sim,n,nh,K,q0,pw,pi,run,alpha0,a,b,powerprior.delta,mc.cores=10)
-# save(powerprior.sim1_9,file='PowerPrior_Sim1_9.RData')
-# 
-# powerprior.sim10_18 <- mclapply(list.scenarios2,PowerPrior_Sim,n,nh,K,q0,pw,pi,run,alpha0,a,b,powerprior.delta,mc.cores=10)
-# save(powerprior.sim10_18,file='PowerPrior_Sim10_18.RData')
-
-
-
-
-#Simulation Across Scenarios-----------------------------
 PowerPrior_Sim <- function(pmat,n,nh,K,q0,pw,pi,run,alpha0,a,b){
   H <- sum(nh!=0)
   p <- pmat[1:K]
@@ -205,106 +44,64 @@ nh <- c(13,13,13,0,0)
 q0 <- 0.1
 run <- 5000
 pw <- 0.2
-alpha0 <- 1
+alpha0 <- 0.5
 a <- 1
 b <- 1
 pi <- rbind(c(0.5,0.5),c(0.5,0.5),c(0.5,0.5),c(0.5,0.5),c(0.5,0.5))
 
-p1.1 <- c(0.1,0.1,0.1,0.1,0.1,1,1,1)
-p2.1 <- c(0.25,0.1,0.1,0.1,0.1,1,1,1)
-p3.1 <- c(0.25,0.25,0.1,0.1,0.1,1,1,1)
-p4.1 <- c(0.25,0.25,0.25,0.1,0.1,1,1,1)
-p5.1 <- c(0.25,0.25,0.25,0.25,0.1,1,1,1)
-p6.1 <- c(0.25,0.25,0.25,0.25,0.25,1,1,1)
-p1.2 <- c(0.1,0.1,0.1,0.1,0.1,3,1,1)
-p2.2 <- c(0.25,0.1,0.1,0.1,0.1,3,1,1)
-p3.2 <- c(0.25,0.25,0.1,0.1,0.1,3,1,1)
-p4.2 <- c(0.25,0.25,0.25,0.1,0.1,3,1,1)
-p5.2 <- c(0.25,0.25,0.25,0.25,0.1,3,1,1)
-p6.2 <- c(0.25,0.25,0.25,0.25,0.25,3,1,1)
-p1.3 <- c(0.1,0.1,0.1,0.1,0.1,3,3,1)
-p2.3 <- c(0.25,0.1,0.1,0.1,0.1,3,3,1)
-p3.3 <- c(0.25,0.25,0.1,0.1,0.1,3,3,1)
-p4.3 <- c(0.25,0.25,0.25,0.1,0.1,3,3,1)
-p5.3 <- c(0.25,0.25,0.25,0.25,0.1,3,3,1)
-p6.3 <- c(0.25,0.25,0.25,0.25,0.25,3,3,1)
-p1.4 <- c(0.1,0.1,0.1,0.1,0.1,3,3,3)
-p2.4 <- c(0.25,0.1,0.1,0.1,0.1,3,3,3)
-p3.4 <- c(0.25,0.25,0.1,0.1,0.1,3,3,3)
-p4.4 <- c(0.25,0.25,0.25,0.1,0.1,3,3,3)
-p5.4 <- c(0.25,0.25,0.25,0.25,0.1,3,3,3)
-p6.4 <- c(0.25,0.25,0.25,0.25,0.25,3,3,3)
-p7.1 <- c(0.1,0.1,0.1,0.25,0.1,1,1,1)
-p7.2 <- c(0.1,0.1,0.1,0.25,0.1,3,1,1)
-p7.3 <- c(0.1,0.1,0.1,0.25,0.1,3,3,1)
-p7.4 <- c(0.1,0.1,0.1,0.25,0.1,3,3,3)
-p8.1 <- c(0.25,0.1,0.1,0.25,0.1,1,1,1)
-p8.2 <- c(0.25,0.1,0.1,0.25,0.1,3,1,1)
-p8.3 <- c(0.25,0.1,0.1,0.25,0.1,3,3,1)
-p8.4 <- c(0.25,0.1,0.1,0.25,0.1,3,3,3)
+p1 <- c(0.1,0.1,0.1,0.1,0.1,1,1,1)
+p2 <- c(0.25,0.1,0.1,0.1,0.1,1,1,1)
+p3 <- c(0.25,0.25,0.1,0.1,0.1,1,1,1)
+p4 <- c(0.25,0.25,0.25,0.1,0.1,1,1,1)
+p5 <- c(0.25,0.25,0.25,0.25,0.1,1,1,1)
+p6 <- c(0.25,0.25,0.25,0.25,0.25,1,1,1)
+p7 <- c(0.1,0.1,0.1,0.25,0.1,1,1,1)
+p8 <- c(0.25,0.1,0.1,0.25,0.1,1,1,1)
+ 
+list.scenarios1 <- list(p1,p2,p3,p4,p5,p6,p7,p8)
+powerprior.sim0 <- mclapply(list.scenarios1,PowerPrior_Sim,n,nh,K,q0,pw,pi,run,alpha0,a,b,mc.cores=9)
+save(powerprior.sim0,file='PowerPrior1_Sim0.RData')
+ 
+p1 <- c(0.1,0.1,0.1,0.1,0.1,3,1,1)
+p2 <- c(0.25,0.1,0.1,0.1,0.1,3,1,1)
+p3 <- c(0.25,0.25,0.1,0.1,0.1,3,1,1)
+p4 <- c(0.25,0.25,0.25,0.1,0.1,3,1,1)
+p5 <- c(0.25,0.25,0.25,0.25,0.1,3,1,1)
+p6 <- c(0.25,0.25,0.25,0.25,0.25,3,1,1)
+p7 <- c(0.1,0.1,0.1,0.25,0.1,3,1,1)
+p8 <- c(0.25,0.1,0.1,0.25,0.1,3,1,1)
+ 
+list.scenarios2 <- list(p1,p2,p3,p4,p5,p6,p7,p8)
+powerprior.sim1 <- mclapply(list.scenarios2,PowerPrior_Sim,n,nh,K,q0,pw,pi,run,alpha0,a,b,mc.cores=9)
+save(powerprior.sim1,file='PowerPrior1_Sim1.RData')
+ 
+p1 <- c(0.1,0.1,0.1,0.1,0.1,3,3,1)
+p2 <- c(0.25,0.1,0.1,0.1,0.1,3,3,1)
+p3 <- c(0.25,0.25,0.1,0.1,0.1,3,3,1)
+p4 <- c(0.25,0.25,0.25,0.1,0.1,3,3,1)
+p5 <- c(0.25,0.25,0.25,0.25,0.1,3,3,1)
+p6 <- c(0.25,0.25,0.25,0.25,0.25,3,3,1)
+p7 <- c(0.1,0.1,0.1,0.25,0.1,3,3,1)
+p8 <- c(0.25,0.1,0.1,0.25,0.1,3,3,1)
+ 
+list.scenarios3 <- list(p1,p2,p3,p4,p5,p6,p7,p8)
+powerprior.sim2 <- mclapply(list.scenarios3,PowerPrior_Sim,n,nh,K,q0,pw,pi,run,alpha0,a,b,mc.cores=9)
+save(powerprior.sim2,file='PowerPrior1_Sim2.RData')
+ 
+p1 <- c(0.1,0.1,0.1,0.1,0.1,3,3,3)
+p2 <- c(0.25,0.1,0.1,0.1,0.1,3,3,3)
+p3 <- c(0.25,0.25,0.1,0.1,0.1,3,3,3)
+p4 <- c(0.25,0.25,0.25,0.1,0.1,3,3,3)
+p5 <- c(0.25,0.25,0.25,0.25,0.1,3,3,3)
+p6 <- c(0.25,0.25,0.25,0.25,0.25,3,3,3)
+p7 <- c(0.1,0.1,0.1,0.25,0.1,3,3,3)
+p8 <- c(0.25,0.1,0.1,0.25,0.1,3,3,3)
+ 
+list.scenarios4 <- list(p1,p2,p3,p4,p5,p6,p7,p8)
+powerprior.sim3 <- mclapply(list.scenarios4,PowerPrior_Sim,n,nh,K,q0,pw,pi,run,alpha0,a,b,mc.cores=9)
+save(powerprior.sim3,file='PowerPrior1_Sim3.RData')
 
 
-list.scenarios1 <- list(p1.1,p2.1,p3.1,p4.1,p5.1,p6.1,p7.1,p8.1,
-                        p1.2,p2.2,p3.2,p4.2,p5.2,p6.2,p7.2,p8.2,
-                        p1.3,p2.3,p3.3,p4.3,p5.3,p6.3,p7.3,p8.3,
-                        p1.4,p2.4,p3.4,p4.4,p5.4,p6.4,p7.4,p8.4)
-
-powerprior.sim1 <- mclapply(list.scenarios1,PowerPrior_Sim,n,nh,K,q0,pw,pi,run,alpha0,a,b,mc.cores=33)
-save(powerprior.sim1,file='PowerPrior1_Sim.RData')
-# 
-# p1 <- c(0.1,0.1,0.1,0.1,0.1,1,1,1)
-# p2 <- c(0.25,0.1,0.1,0.1,0.1,1,1,1)
-# p3 <- c(0.25,0.25,0.1,0.1,0.1,1,1,1)
-# p4 <- c(0.25,0.25,0.25,0.1,0.1,1,1,1)
-# p5 <- c(0.25,0.25,0.25,0.25,0.1,1,1,1)
-# p6 <- c(0.25,0.25,0.25,0.25,0.25,1,1,1)
-# p7 <- c(0.1,0.1,0.1,0.25,0.1,1,1,1)
-# p8 <- c(0.25,0.1,0.1,0.25,0.1,1,1,1)
-# 
-# list.scenarios1 <- list(p1,p2,p3,p4,p5,p6,p7,p8)
-# powerprior.sim0 <- mclapply(list.scenarios1,PowerPrior_Sim,n,nh,K,q0,pw,pi,run,alpha0,a,b,mc.cores=9)
-# save(powerprior.sim0,file='PowerPrior1_Sim0.RData')
-# 
-# p1 <- c(0.1,0.1,0.1,0.1,0.1,3,1,1)
-# p2 <- c(0.25,0.1,0.1,0.1,0.1,3,1,1)
-# p3 <- c(0.25,0.25,0.1,0.1,0.1,3,1,1)
-# p4 <- c(0.25,0.25,0.25,0.1,0.1,3,1,1)
-# p5 <- c(0.25,0.25,0.25,0.25,0.1,3,1,1)
-# p6 <- c(0.25,0.25,0.25,0.25,0.25,3,1,1)
-# p7 <- c(0.1,0.1,0.1,0.25,0.1,3,1,1)
-# p8 <- c(0.25,0.1,0.1,0.25,0.1,3,1,1)
-# 
-# list.scenarios2 <- list(p1,p2,p3,p4,p5,p6,p7,p8)
-# powerprior.sim1 <- mclapply(list.scenarios2,PowerPrior_Sim,n,nh,K,q0,pw,pi,run,alpha0,a,b,mc.cores=9)
-# save(powerprior.sim1,file='PowerPrior1_Sim1.RData')
-# 
-# p1 <- c(0.1,0.1,0.1,0.1,0.1,3,3,1)
-# p2 <- c(0.25,0.1,0.1,0.1,0.1,3,3,1)
-# p3 <- c(0.25,0.25,0.1,0.1,0.1,3,3,1)
-# p4 <- c(0.25,0.25,0.25,0.1,0.1,3,3,1)
-# p5 <- c(0.25,0.25,0.25,0.25,0.1,3,3,1)
-# p6 <- c(0.25,0.25,0.25,0.25,0.25,3,3,1)
-# p7 <- c(0.1,0.1,0.1,0.25,0.1,3,3,1)
-# p8 <- c(0.25,0.1,0.1,0.25,0.1,3,3,1)
-# 
-# list.scenarios3 <- list(p1,p2,p3,p4,p5,p6,p7,p8)
-# powerprior.sim2 <- mclapply(list.scenarios3,PowerPrior_Sim,n,nh,K,q0,pw,pi,run,alpha0,a,b,mc.cores=9)
-# save(powerprior.sim2,file='PowerPrior1_Sim2.RData')
-# 
-# p1 <- c(0.1,0.1,0.1,0.1,0.1,3,3,3)
-# p2 <- c(0.25,0.1,0.1,0.1,0.1,3,3,3)
-# p3 <- c(0.25,0.25,0.1,0.1,0.1,3,3,3)
-# p4 <- c(0.25,0.25,0.25,0.1,0.1,3,3,3)
-# p5 <- c(0.25,0.25,0.25,0.25,0.1,3,3,3)
-# p6 <- c(0.25,0.25,0.25,0.25,0.25,3,3,3)
-# p7 <- c(0.1,0.1,0.1,0.25,0.1,3,3,3)
-# p8 <- c(0.25,0.1,0.1,0.25,0.1,3,3,3)
-# 
-# list.scenarios4 <- list(p1,p2,p3,p4,p5,p6,p7,p8)
-# powerprior.sim3 <- mclapply(list.scenarios4,PowerPrior_Sim,n,nh,K,q0,pw,pi,run,alpha0,a,b,mc.cores=9)
-# save(powerprior.sim3,file='PowerPrior1_Sim3.RData')
-
-# 
 # #Extra Scenarios
 # p9 <- c(0.25,0.25,0.1,0.25,0.1,1,1,1)
 # p10 <- c(0.1,0.1,0.1,0.25,0.25,1,1,1)
@@ -343,9 +140,7 @@ save(powerprior.sim1,file='PowerPrior1_Sim.RData')
 # extra.powerprior.sim3 <- mclapply(extra.list.scenarios4,PowerPrior_Sim,n,nh,K,q0,pw,pi,run,alpha0,a,b,mc.cores=5)
 # save(extra.powerprior.sim3,file='PowerPrior_Sim3_Extra.RData')
 
-# 
-# 
-# 
+
 #Results Analysis---------------------------------------------------
 # calmat <- 0
 # for(i in 1:5){
